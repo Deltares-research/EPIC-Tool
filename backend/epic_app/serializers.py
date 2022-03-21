6c6df1acadad989b63d0a7532e19c7da753a51d9
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-03-21 15:01:44 +0100

    feat(serializers.py): Added JSON serializers
    Required step to generate retrievable information through REST API calls.

---
+++ backend/epic_app/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from epic_app.models import EpicUser, UserQuestionAnswers, QuestionAnswerForm, Question, Answer
+
+class EpicUserSerializer(serializers.HyperlinkedModelSerializer):'
+    """
+    Serializer for 'EpicUser'
+    """
+    class Meta:
+        model=EpicUser
+        fields = ('username', 'organization')
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer for 'Question'
+    """
+    class Meta:
+        model=Question
+        fields = ('description')
+    
+class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer for 'Answer'
+    """
+    class Meta:
+        model=Answer
+        fields = ('short_answer', 'long_answer')
+
+class QuestionAnswerFormSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer for 'QuestionAnswerForm'
+    """
+    class Meta:
+        model=QuestionAnswerForm
+        fields = ('user', 'qa_form')
+
+class UserQuestionAnswersSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer for 'UserQuestionAnswers'
+    """
+    class Meta:
+        model=UserQuestionAnswers
+        fields = ('user', 'qa_form')
