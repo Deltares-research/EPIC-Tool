3f0b9e1a9989ed23d50fe106b37766b7da7cf218
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-04-04 09:42:47 +0200

    feat(urls.py): Added redirection for the admin page

--- backend/epic_app/urls.py
+++ backend/epic_app/urls.py
@@ -2,7 +2,7 @@ from django.views.generic.base import RedirectView
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-from epic_app import views
+from epic_app import views, apps
 
 router = routers.DefaultRouter()
 router.register(r'epicuser', views.EpicUserViewSet)
@@ -16,7 +16,9 @@ router.register(r'answer', views.AnswerViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', RedirectView.as_view(url='api/', permanent=False), name='index'),
-    path('api/', include(router.urls)),
+    path(f'admin/{apps.EpicAppConfig.name}', RedirectView.as_view(url='api/', permanent=False), name=f'admin_{apps.EpicAppConfig.name}'),
+
+    path('api/', include(router.urls), name='api'),
     path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
