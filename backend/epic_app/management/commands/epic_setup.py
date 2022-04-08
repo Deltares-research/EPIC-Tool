08ab7f57e1a33a47d4b4673d1d91bc5503118a48
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-04-08 09:40:06 +0200

    chore(epic_setup.py): applied isort & black

--- backend/epic_app/management/commands/epic_setup.py
+++ backend/epic_app/management/commands/epic_setup.py
@@ -1,7 +1,9 @@
+from pathlib import Path
 from typing import Any, Optional
+
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
-from pathlib import Path
+
 
 class Command(BaseCommand):
     help = "Sets the default EPIC database with a predefined admin user. If the database already exists then it removes it (and its migrations) and creates one from zero."
@@ -16,7 +18,9 @@ class Command(BaseCommand):
         migrations_dir = self.epic_app_dir / "migrations"
         for m_file in migrations_dir.glob("*.py"):
             if m_file.name != "__init__.py":
-                self.stdout.write(self.style.NOTICE(f"Removing migration file: {m_file.name}")) 
+                self.stdout.write(
+                    self.style.NOTICE(f"Removing migration file: {m_file.name}")
+                )
                 m_file.unlink()
 
     def _cleanup_db(self):
@@ -25,10 +29,12 @@ class Command(BaseCommand):
         """
         db_path = self.root_dir / "db.sqlite3"
         if db_path.is_file():
-            self.stdout.write(self.style.NOTICE(f"Removing database file at {db_path}")) 
+            self.stdout.write(self.style.NOTICE(f"Removing database file at {db_path}"))
             db_path.unlink()
         self._remove_migrations()
-        self.stdout.write(self.style.SUCCESS("Successfully cleaned up previous database structure.")) 
+        self.stdout.write(
+            self.style.SUCCESS("Successfully cleaned up previous database structure.")
+        )
 
     def _migrate_db(self):
         """
@@ -38,10 +44,11 @@ class Command(BaseCommand):
         call_command("migrate")
         call_command("createsuperuser")
 
-
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         try:
             self._cleanup_db()
             self._migrate_db()
         except Exception as e_info:
-            self.stdout.write(self.style.ERROR(f"Error setting up EPIC. Detailed info: {str(e_info)}"))
+            self.stdout.write(
+                self.style.ERROR(f"Error setting up EPIC. Detailed info: {str(e_info)}")
+            )
