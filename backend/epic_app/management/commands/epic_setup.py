a2ca31e75424532a7e8806cde83743b1dd8e506f
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-04-09 20:58:26 +0200

    ci(epic_app/management/commands/epic_setup.py): Extended setup to automatically import csv data and create users (including superuser)

--- backend/epic_app/management/commands/epic_setup.py
+++ backend/epic_app/management/commands/epic_setup.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 from typing import Any, Optional
 
+from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
+
+from epic_app.importers import EpicAgencyImporter, EpicDomainImporter
+from epic_app.models.models import EpicUser
 
 
 class Command(BaseCommand):
@@ -19,7 +23,7 @@ class Command(BaseCommand):
         for m_file in migrations_dir.glob("*.py"):
             if m_file.name != "__init__.py":
                 self.stdout.write(
-                    self.style.NOTICE(f"Removing migration file: {m_file.name}")
+                    self.style.WARNING(f"Removing migration file: {m_file.name}")
                 )
                 m_file.unlink()
 
@@ -29,12 +33,81 @@ class Command(BaseCommand):
         """
         db_path = self.root_dir / "db.sqlite3"
         if db_path.is_file():
-            self.stdout.write(self.style.NOTICE(f"Removing database file at {db_path}"))
+            self.stdout.write(
+                self.style.WARNING(f"Removing database file at {db_path}")
+            )
             db_path.unlink()
         self._remove_migrations()
         self.stdout.write(
             self.style.SUCCESS("Successfully cleaned up previous database structure.")
         )
+
+    def _import_files(self, test_data_dir: Path):
+        epic_domain_csv = test_data_dir / "initial_epic_data.csv"
+        if epic_domain_csv.is_file():
+            self.stdout.write(
+                self.style.MIGRATE_HEADING(
+                    f"Importing main data from {epic_domain_csv}."
+                )
+            )
+            EpicDomainImporter().import_csv(epic_domain_csv)
+            self.stdout.write(self.style.SUCCESS("Import successful."))
+        epic_agency_csv = test_data_dir / "agency_data.csv"
+        if epic_agency_csv.is_file():
+            self.stdout.write(
+                self.style.MIGRATE_HEADING(
+                    f"Importing agencies from {epic_agency_csv}."
+                )
+            )
+            EpicAgencyImporter().import_csv(epic_agency_csv)
+            self.stdout.write(self.style.SUCCESS("Import successful."))
+
+    def _create_superuser(self):
+        # Create an admin user.
+        admin_user = User(
+            username="admin",
+            email="admin@testdb.com",
+            first_name="Star",
+            last_name="Lord",
+        )
+        admin_user.set_password("admin")
+        admin_user.is_superuser = True
+        admin_user.is_staff = True
+        admin_user.save()
+        self.stdout.write(
+            self.style.SUCCESS("Created superuser: 'admin', password: 'admin'.")
+        )
+
+    def _create_dummy_users(self):
+        try:
+            self._create_superuser()
+        except:
+            call_command("createsuperuser")
+
+        # Create a few basic users.
+        EpicUser.objects.create(username="Zelda", organization="Nintendo")
+        EpicUser.objects.create(username="Ganon", organization="Nintendo")
+        EpicUser.objects.create(username="Luke", organization="Rebel Alliance")
+        EpicUser.objects.create(username="Leia", organization="Rebel Alliance")
+
+    def _import_test_db(self):
+        test_data_dir: Path = self.epic_app_dir / "tests" / "test_data"
+        if not test_data_dir.is_dir():
+            self.stdout.write(
+                self.style.ERROR(
+                    f"No test data found at {test_data_dir}, database will be empty on start."
+                )
+            )
+        try:
+            self._import_files(test_data_dir)
+            self._create_dummy_users()
+        except Exception as e_info:
+            call_command("flush")
+            self.stdout.write(
+                self.style.ERROR(
+                    f"Could not correctly import test data, database will be empty on start. Detail error: {str(e_info)}."
+                )
+            )
 
     def _migrate_db(self):
         """
@@ -42,7 +115,7 @@ class Command(BaseCommand):
         """
         call_command("makemigrations")
         call_command("migrate")
-        call_command("createsuperuser")
+        self._import_test_db()
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         try:
