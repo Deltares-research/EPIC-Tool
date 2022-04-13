d8f7f60b0118e8d8fe3929a22eb7f7c794c6263d
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-04-13 21:32:10 +0200

    test(test_views.py) Created test file for viewsets (urls)

---
+++ backend/epic_app/tests/test_views.py
@@ -0,0 +1,22 @@
+class TestEpicUserViewSet:
+    pass
+
+
+class TestAreaViewSet:
+    pass
+
+
+class TestAgencyViewSet:
+    pass
+
+
+class TestGroupoViewSet:
+    pass
+
+
+class TestProgramViewSet:
+    pass
+
+
+class TestAnswerViewSet:
+    pass
