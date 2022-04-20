ce4a0299873cda8c4ebc9920913a8d34a22f55f9
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-04-20 10:42:10 +0200

    test(epic_app/tests/models/test_epic_answers.py): Small refactor to improve maintainance

--- backend/epic_app/tests/models/test_epic_answers.py
+++ backend/epic_app/tests/models/test_epic_answers.py
@@ -1,3 +1,6 @@
+import itertools
+from typing import List
+
 import pytest
 from django.db import IntegrityError
 
@@ -8,16 +11,8 @@ from epic_app.models.epic_answers import (
     YesNoAnswer,
     YesNoAnswerType,
 )
-from epic_app.models.epic_questions import (
-    EvolutionChoiceType,
-    EvolutionQuestion,
-    KeyAgencyActionsQuestion,
-    LinkagesQuestion,
-    NationalFrameworkQuestion,
-    Question,
-)
+from epic_app.models.epic_questions import EvolutionChoiceType, Question
 from epic_app.models.epic_user import EpicUser
-from epic_app.models.models import Program
 from epic_app.tests.epic_db_fixture import epic_test_db
 
 
@@ -32,169 +27,66 @@ def EpicAnswersFixture(epic_test_db: pytest.fixture):
     pass
 
 
-# @pytest.mark.django_db
-# class TestEpicAnswer:
-#     @pytest.mark.parametrize(
-#         "q_type",
-#         [
-#             pytest.param(NationalFrameworkQuestion, id="NationalFrameworkQuestion"),
-#             pytest.param(EvolutionQuestion, id="EvolutionQuestion"),
-#             pytest.param(LinkagesQuestion, id="LinkagesQuestion"),
-#         ],
-#     )
-#     def test_get_answer_returns_new_instance_when_doesnot_exist(self, q_type: Question):
-#         q_instance: Question = q_type.objects.all().last()
-#         u_question: EpicUser = EpicUser.objects.all().last()
-#         assert not Answer.objects.filter(user=u_question, question=q_instance).exists()
-
-#         # Try to get the answer for the first time.
-#         nf_answer: Answer = q_instance.get_answer(u_question)
-#         assert Answer.objects.filter(user=u_question, question=q_instance).exists()
-
-#         # Try to get it again
-#         nf_answer_two: Answer = q_instance.get_answer(u_question)
-#         assert nf_answer == nf_answer_two
-@pytest.mark.django_db
-class TestYesNoAnswer:
-    @pytest.mark.parametrize(
-        "unsupported_type",
-        [pytest.param(EvolutionQuestion), pytest.param(LinkagesQuestion)],
-    )
-    def test_SAVE_unsupported_type_raises_integrityerror(
-        self, unsupported_type: Question
-    ):
-        # Define test data
-        question_instance = unsupported_type.objects.all().first()
-        assert isinstance(question_instance, Question)
-
-        # Run test
-        with pytest.raises(IntegrityError) as ie_exc:
-            YesNoAnswer.objects.create(
-                short_answer=YesNoAnswerType.NO,
-                justify_answer="Magna laboris ex Lorem dolor mollit eiusmod occaecat deserunt reprehenderit labore velit ad excepteur.",
-                question=question_instance,
-                user=EpicUser.objects.all().first(),
-            )
-
-        # Verify final expectations.
-        assert (
-            str(ie_exc.value)
-            == f"Answer type {unsupported_type.__name__} not allowed. Supported types: NationalFrameworkQuestion, KeyAgencyActionsQuestion"
-        )
-
-    @pytest.mark.parametrize(
-        "supported_type",
-        [
-            pytest.param(NationalFrameworkQuestion),
-            pytest.param(KeyAgencyActionsQuestion),
-        ],
-    )
-    def test_SAVE_supported_type_succeeds(self, supported_type: Question):
-        # Define test data
-        question_instance = supported_type.objects.all().first()
-        assert isinstance(question_instance, Question)
-
-        # Run test
-        obj_created = YesNoAnswer.objects.create(
-            short_answer=YesNoAnswerType.NO,
-            justify_answer="Magna laboris ex Lorem dolor mollit eiusmod occaecat deserunt reprehenderit labore velit ad excepteur.",
-            question=question_instance,
-            user=EpicUser.objects.all().first(),
-        )
-
-        # Verify final expectations.
-        assert isinstance(obj_created, YesNoAnswer)
+def get_subtypes(model):
+    subtypes = []
+    for m in model.__subclasses__():
+        if m.__subclasses__():
+            subtypes.append(m.__subclasses__())
+        else:
+            subtypes.append([m])
+    return list(itertools.chain(*subtypes))
 
 
 @pytest.mark.django_db
-class TestSingleChoiceAnswer:
-    @pytest.mark.parametrize(
-        "unsupported_type",
-        [
-            pytest.param(LinkagesQuestion),
-            pytest.param(NationalFrameworkQuestion),
-            pytest.param(KeyAgencyActionsQuestion),
-        ],
-    )
-    def test_SAVE_unsupported_type_raises_integrityerror(
-        self, unsupported_type: Question
-    ):
-        # Define test data
-        question_instance = unsupported_type.objects.all().first()
-        assert isinstance(question_instance, Question)
+class TestEpicAnswers:
 
-        # Run test
-        with pytest.raises(IntegrityError) as ie_exc:
-            SingleChoiceAnswer.objects.create(
-                selected_choice=EvolutionChoiceType.NASCENT,
-                justify_answer="Magna laboris ex Lorem dolor mollit eiusmod occaecat deserunt reprehenderit labore velit ad excepteur.",
-                question=question_instance,
-                user=EpicUser.objects.all().first(),
-            )
-
-        # Verify final expectations.
-        assert (
-            str(ie_exc.value)
-            == f"Answer type {unsupported_type.__name__} not allowed. Supported types: EvolutionQuestion"
-        )
-
-    def test_SAVE_supported_type_succeeds(self):
-        # Define test data
-        question_instance = EvolutionQuestion.objects.all().first()
-        assert isinstance(question_instance, Question)
-
-        # Run test
-        obj_created = SingleChoiceAnswer.objects.create(
+    answer_ctor: dict = {
+        MultipleChoiceAnswer: dict(),
+        SingleChoiceAnswer: dict(
             selected_choice=EvolutionChoiceType.NASCENT,
             justify_answer="Magna laboris ex Lorem dolor mollit eiusmod occaecat deserunt reprehenderit labore velit ad excepteur.",
-            question=question_instance,
-            user=EpicUser.objects.all().first(),
-        )
+        ),
+        YesNoAnswer: dict(
+            short_answer=YesNoAnswerType.NO,
+            justify_answer="Magna laboris ex Lorem dolor mollit eiusmod occaecat deserunt reprehenderit labore velit ad excepteur.",
+        ),
+    }
 
-        # Verify final expectations.
-        assert isinstance(obj_created, SingleChoiceAnswer)
-
-
-@pytest.mark.django_db
-class TestMultipleChoiceAnswer:
     @pytest.mark.parametrize(
-        "unsupported_type",
-        [
-            pytest.param(EvolutionQuestion),
-            pytest.param(NationalFrameworkQuestion),
-            pytest.param(KeyAgencyActionsQuestion),
-        ],
+        "question_subtype",
+        get_subtypes(Question),
     )
-    def test_SAVE_unsupported_type_raises_integrityerror(
-        self, unsupported_type: Question
-    ):
+    @pytest.mark.parametrize("answer_subtype", get_subtypes(Answer))
+    def test_SAVE_answer(self, question_subtype: Question, answer_subtype: Answer):
         # Define test data
-        question_instance = unsupported_type.objects.all().first()
-        assert isinstance(question_instance, Question)
+        if not answer_subtype in self.answer_ctor.keys():
+            # Check the key exists (doing a .get(key, None) would always check to false for linkages)
+            pytest.fail(f"No test input defined for answer type: {answer_subtype}")
 
-        # Run test
-        with pytest.raises(IntegrityError) as ie_exc:
-            MultipleChoiceAnswer.objects.create(
-                question=question_instance,
-                user=EpicUser.objects.all().first(),
+        answer_args = self.answer_ctor[answer_subtype]
+        answer_args["question"] = question_subtype.objects.all().first()
+        answer_args["user"] = EpicUser.objects.all().first()
+        assert isinstance(answer_args["question"], Question)
+
+        # Run test and verify expectations.
+        answer_instance: Answer = answer_subtype(**answer_args)
+        assert not answer_instance in list(answer_subtype.objects.all())
+
+        if question_subtype in answer_instance._get_supported_questions():
+            # Run test
+            answer_instance.save()
+            # Verify final expectations.
+            assert answer_instance in list(answer_subtype.objects.all())
+        else:
+            # Run test
+            with pytest.raises(IntegrityError) as ie_exc:
+                answer_instance.save()
+
+            # Verify final expectations.
+            supported_answers: str = ", ".join(
+                [sq.__name__ for sq in answer_instance._get_supported_questions()]
             )
-
-        # Verify final expectations.
-        assert (
-            str(ie_exc.value)
-            == f"Answer type {unsupported_type.__name__} not allowed. Supported types: LinkagesQuestion"
-        )
-
-    def test_SAVE_supported_type_succeeds(self):
-        # Define test data
-        question_instance = LinkagesQuestion.objects.all().first()
-        assert isinstance(question_instance, Question)
-
-        # Run test
-        obj_created = MultipleChoiceAnswer.objects.create(
-            question=question_instance,
-            user=EpicUser.objects.all().first(),
-        )
-
-        # Verify final expectations.
-        assert isinstance(obj_created, MultipleChoiceAnswer)
+            assert (
+                str(ie_exc.value)
+                == f"Answer type {question_subtype.__name__} not allowed. Supported types: {supported_answers}"
+            )
