a62a456a896e2badca91891dcfa68ea14fcc4d66
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-05-03 14:13:19 +0200

    test(test_rest_framework_url.py): Fixed failing test

--- backend/epic_app/tests/test_rest_framework_url.py
+++ backend/epic_app/tests/test_rest_framework_url.py
@@ -734,7 +734,7 @@ class TestAnswerViewSet:
             ),
             pytest.param(
                 singlechoice_url,
-                dict(question="3", selected_choice="ENGAGED"),
+                dict(question="3", selected_choice=EvolutionChoiceType.ENGAGED),
                 id="SingleChoice answer",
             ),
             pytest.param(
