9eb48095318a8efb7c9d11bcc437bf8bf7478528
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-05-23 14:50:09 +0200

    ci(deploy.sh): Changed script to run on python3

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Author : Carles S. Soriano Perez (carles.sorianoperez@deltares.nl)
-python -c "import secrets; from pathlib import Path; Path('.django_secrets').write_text(secrets.token_hex(16))"
-python -c "from pathlib import Path; Path('.django_debug').write_text('False')"
+python3 -c "import secrets; from pathlib import Path; Path('.django_secrets').write_text(secrets.token_hex(16))"
+python3 -c "from pathlib import Path; Path('.django_debug').write_text('False')"
 poetry run python3 manage.py epic_setup
 poetry run python3 manage.py collectstatic --noinput
 poetry run gunicorn epic_setup.wsgi &
