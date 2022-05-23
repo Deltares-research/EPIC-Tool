b377f8f5d0af9fa03e257e2cdaa6b865667a9b15
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-05-23 12:38:51 +0200

    ci(settings.py): Generated static files

--- backend/epic_core/settings.py
+++ backend/epic_core/settings.py
@@ -150,6 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = "/var/www/html/ighcrm/static/"
 STATIC_URL = "static/"
 
 # Default primary key field type
