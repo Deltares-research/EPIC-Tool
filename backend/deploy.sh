bc4799a05325173f5d2cca2110e7fc7846de0fb6
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-05-23 14:40:15 +0200

    ci: Added collect static to shell script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,5 +3,6 @@
 # Author : Carles S. Soriano Perez (carles.sorianoperez@deltares.nl)
 python -c "import secrets; from pathlib import Path; Path('.django_secrets').write_text(secrets.token_hex(16))"
 python -c "from pathlib import Path; Path('.django_debug').write_text('False')"
-poetry run python manage.py epic_setup
+poetry run python3 manage.py epic_setup
+poetry run python3 manage.py collectstatic --noinput
 poetry run gunicorn epic_setup.wsgi &
