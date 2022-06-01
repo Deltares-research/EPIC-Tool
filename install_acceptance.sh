ac518d51d285aa4bbaf63e5bb96c8e13a5f5eb09
grijzea <80118647+grijzea@users.noreply.github.com>
2022-06-01 11:50:48 +0200

    install script (#80)

---
+++ install_acceptance.sh
@@ -0,0 +1,4 @@
+cd /var/www/epictool-site
+npm install
+npm run acceptance
+cp -R dist/* /var/www/html/ighcrm
---
+++ install_preprod.sh
@@ -0,0 +1,4 @@
+cd /var/www/epictool-site
+npm install
+npm run preprod
+cp -R dist/* /var/www/html/ighcrm
