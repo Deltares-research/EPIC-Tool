8282614eea1377ff8fcfbfc8eea1b60a45f5af3b
IoannaMi <ioannamicha@hotmail.com>
2024-06-21 17:03:56 +0200

    Add [WIP] message if program is not in selectedProgram

--- src/views/EvolutionOverview.vue
+++ src/views/EvolutionOverview.vue
@@ -42,7 +42,8 @@
         <v-col class="custom-center">
           <h4 style="color: darkred; margin-bottom: 30px; max-width: 600px" v-if="this.dataPieLoaded == true">Please review the figure displaying your assessment results. To make adjustments, click on the relevant section to be directed back to the questionnaire. </h4>
           <h3 style="color: darkred; margin-bottom: 30px;" v-if="this.dataPieLoaded == true && this.clickedElementName == ''">Click on the graph to change an element</h3>
-          <v-btn class="primary" style="margin-bottom: 30px;" v-if="this.dataPieLoaded == true && this.clickedElementName !== ''" @click="goToQuestionnaire()">Change {{clickedElementName}}</v-btn>
+          <h3 style="color: darkred; margin-bottom: 30px;" v-if="this.dataPieLoaded == true && !selectedProgramCheck">TEST TEST TEST</h3>
+          <v-btn class="primary" style="margin-bottom: 30px;" v-if="this.dataPieLoaded == true && selectedProgramCheck" @click="goToQuestionnaire()">Change {{clickedElementName}}</v-btn>
           <v-btn class="primary" style="margin-bottom: 30px;" v-if="this.dataPieLoaded == true" @click="generateGraph()">Confirm graph</v-btn>
         </v-col>
 
@@ -96,7 +97,11 @@ export default {
     this.url = "";
   },
   computed: {
-    ...mapState(['programs'])
+    ...mapState(['programs', 'programSelection', 'currentProgram']),
+    selectedProgramCheck() {
+      const arrayPrograms = Array.from(this.programSelection);
+      return arrayPrograms.find(program => program === this.currentProgram.id)
+    }
   },
 
   methods: {
@@ -195,6 +200,7 @@ export default {
       this.clickedElementName = params.name;
       const selectedProgram = this.programs.find(program => program.name === params.name)
       this.updateCurrentProgram(selectedProgram)
+      
     },
     goToQuestionnaire() {
       // Get the current URL
